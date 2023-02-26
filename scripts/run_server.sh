ddca6ea57ea9226a0985449629146d0e5826e46d
Lue Xiong <luexiong@lues-mbp.lan>
2023-02-25 18:11:54 -0600

    Add creation of admin in script before start up

--- scripts/run_server.sh
+++ scripts/run_server.sh
@@ -3,4 +3,6 @@
 set -ex
 
 python manage.py migrate && \
+    DJANGO_SUPERUSER_PASSWORD=admin python manage.py createsuperuser \
+    --no-input --username admin --email foobar@gmail.com && \
     python manage.py runserver 0.0.0.0:8000
